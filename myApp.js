9df85ebc2e5e45a4c7ad62b3e1ace6260678e92e
Cormac Quaid <thequad@gmail.com>
2021-01-26 15:52:33 +0100

    Add json route

--- myApp.js
+++ myApp.js
@@ -13,8 +13,10 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 // Serve Static Assets
 let path = __dirname + '/public';
-
 app.use('/public', express.static(path));
+
+// Serve JSON 
+app.get('/json', (req, res) => res.json({ "message": "Hello json" }))
 
 
 
